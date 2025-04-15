4c7b14192ecd84d60a9778ee25f78f2c84d05edd
Thiago V. Silva <thiagovs23@gmail.com>
2025-04-15 01:55:52 -0300

    Manage experiment window configurations

--- MainWindow.py
+++ MainWindow.py
@@ -40,18 +40,6 @@ class MainWindow(QMainWindow):
         main_layout.setContentsMargins(5, 0, 5, 0)
         main_layout.setSpacing(1)
 
-        # # Video feed group
-        # video_group = QGroupBox("Video Feed")
-        # video_layout = QVBoxLayout()
-        # video_layout.setContentsMargins(1, 1, 1, 1)
-        # video_layout.setSpacing(0)
-        # self.video_label = QLabel()
-        # self.video_label.setFixedSize(Constants.TEXTURE_WIDTH, Constants.TEXTURE_HEIGHT)
-        # self.video_label.setStyleSheet("QLabel { background-color: black; }")
-        # video_layout.addWidget(self.video_label, alignment=Qt.AlignmentFlag.AlignCenter)
-        # video_group.setLayout(video_layout)
-        # main_layout.addWidget(video_group)
-
         # Create main widget and layout
         main_widget = QWidget()
         self.setCentralWidget(main_widget)
@@ -101,6 +89,18 @@ class MainWindow(QMainWindow):
         file_layout.addWidget(self.filename_label)
         file_layout.addWidget(self.select_file_btn)
         addWindow_layout.addLayout(file_layout)
+
+        checkbox_layout = QHBoxLayout()
+        checkbox_layout.setContentsMargins(2, 2, 2, 2) 
+        checkbox_layout.setSpacing(1)
+
+        self.showPreview_cb = QCheckBox("Show Preview:")
+        self.showPreview_cb.setChecked(True)    
+        checkbox_layout.addWidget(self.showPreview_cb)
+        self.saveVideo_cb = QCheckBox("Save Video:")
+        self.saveVideo_cb.setChecked(True)
+        checkbox_layout.addWidget(self.saveVideo_cb)  
+        addWindow_layout.addLayout(checkbox_layout) 
         
         self.add_window_btn = QPushButton("Add Window")
         self.add_window_btn.setFixedHeight(20)
@@ -115,28 +115,33 @@ class MainWindow(QMainWindow):
         experimentResources_layout = QVBoxLayout()
         experimentResources_layout.setContentsMargins(2, 2, 2, 2)
         experimentResources_layout.setSpacing(1)
+
+        experimentButtons_layout = QHBoxLayout()
+        experimentButtons_layout.setContentsMargins(2, 2, 2, 2)
+        self.open_experiment_btn = QPushButton("Open Experiment")
+        self.open_experiment_btn.setFixedHeight(20)
+        self.open_experiment_btn.setEnabled(False)
         self.start_btn = QPushButton("Start Experiment")
         self.start_btn.setFixedHeight(20)
         self.start_btn.setEnabled(False)
-        experimentResources_layout.addWidget(self.start_btn)
-        experimentResources_group.setLayout(experimentResources_layout)
+        self.stop_btn = QPushButton("Stop Experiment")
+        self.stop_btn.setFixedHeight(20)
+        self.stop_btn.setEnabled(False)
+        self.close_btn = QPushButton("Close and Clear Experiment")
+        self.close_btn.setFixedHeight(20)
+        self.close_btn.setEnabled(False)
+        experimentButtons_layout.addWidget(self.open_experiment_btn)
+        experimentButtons_layout.addWidget(self.start_btn)
+        experimentButtons_layout.addWidget(self.stop_btn)
+        experimentButtons_layout.addWidget(self.close_btn)
 
-        # self.close_btn = QPushButton("Close")
-        # self.close_btn.setFixedHeight(20)
-        # capture_layout.addWidget(self.close_btn)
-        # capture_group.setLayout(capture_layout)
+        experimentResources_layout.addLayout(experimentButtons_layout)
+        experimentResources_group.setLayout(experimentResources_layout)
 
         # # Add groups to control panel
         control_layout.addWidget(addWindow_group)
         control_layout.addWidget(experimentResources_group)
         main_layout.addWidget(control_panel)
-
-        # # Status bar
-        # self.statusBar = QStatusBar()
-        # self.statusBar.setFixedHeight(20)
-        # self.setStatusBar(self.statusBar)
-        # self.fps_label = QLabel("FPS: 0.0")
-        # self.statusBar.addPermanentWidget(self.fps_label)
 
         # Initialize variables
         self.filename = None
@@ -147,27 +152,19 @@ class MainWindow(QMainWindow):
         
         # Criação de threads para abrir janelas
         self.threads = []
-
-        # # Create video thread
-        # self.thread = VideoThread()
-        # self.thread.frame_ready.connect(self.update_frame)
-        # self.thread.fps_updated.connect(self.update_fps)
-        # self.thread.landmarks_ready.connect(self.save_landmarks)
+        self.windows = []  # List to keep track of opened windows
 
         # # Connect signals
         self.select_file_btn.clicked.connect(self.select_file)
         self.add_window_btn.clicked.connect(self.add_window)
+        self.open_experiment_btn.clicked.connect(self.open_experiment)
         self.start_btn.clicked.connect(self.start_experiment)
-        # self.start_btn.clicked.connect(self.start_capture)
-        # self.stop_btn.clicked.connect(self.stop_capture)
-        # self.close_btn.clicked.connect(self.close)
-        # self.refresh_cameras_btn.clicked.connect(self.refresh_cameras)
-        # self.camera_combo.currentIndexChanged.connect(self.camera_changed)
-        # self.show_landmarks_cb.stateChanged.connect(self.toggle_landmarks)
-        # self.model_complexity_combo.currentIndexChanged.connect(self.update_model_complexity)
+        self.stop_btn.clicked.connect(self.stop_experiment)
+        self.close_btn.clicked.connect(self.close_experiment)
+        self.refresh_cameras_btn.clicked.connect(self.refresh_cameras)
+        self.camera_combo.currentIndexChanged.connect(self.generate_filename)  
 
         # Initial UI state
-        # self.stop_btn.setEnabled(False)
         self.generate_filename()
 
         # Set window size and show
@@ -186,11 +183,23 @@ class MainWindow(QMainWindow):
         if filename:
             self.filename = filename
             self.filename_label.setText(f"File: {os.path.basename(filename)}")
-            self.start_btn.setEnabled(True)
+            self.open_experiment_btn.setEnabled(True)
 
     def add_window(self):
         """Fill a ExperimentWindow object with the selected camera and file and create it on CurrentExperiment group."""
-        experimentWindow = ExperimentWindow(self.camera_combo.currentText(), self.filename, showPreview=True)
+        textureWidth = Constants.TEXTURE_WIDTH
+        textureHeight = Constants.TEXTURE_HEIGHT
+        if not self.showPreview_cb.isChecked():
+            textureWidth = 500
+            textureHeight = 250
+
+        experimentWindow = ExperimentWindow(self.camera_combo.currentText(), 
+                                            self.camera_combo.currentIndex(), 
+                                            self.filename, 
+                                            self.showPreview_cb.isChecked(),
+                                            self.saveVideo_cb.isChecked(),
+                                            textureWidth,
+                                            textureHeight)
 
         isExperimentValid = self.CheckExperimentWindow(experimentWindow)
         if not isExperimentValid:
@@ -207,8 +216,8 @@ class MainWindow(QMainWindow):
         experiment_label.setFixedHeight(20)
 
         # Add the QLabel to the experimentResources_group layout
-        self.start_btn.setEnabled(True)
-        experimentResources_layout = self.start_btn.parentWidget().layout()
+        self.open_experiment_btn.setEnabled(True)
+        experimentResources_layout = self.open_experiment_btn.parentWidget().layout()
         experimentResources_layout.addWidget(experiment_label)      
 
     def CheckExperimentWindow(self, experimentWindow):
@@ -226,39 +235,86 @@ class MainWindow(QMainWindow):
                     return False
         return True
 
-    def start_experiment(self):
-        """Start the experiment by opening all ExperimentWindow objects."""
-
-        windows = []  # List to keep track of opened windows
+    def open_experiment(self):
+        """Open the experiment by opening all ExperimentWindow objects."""
                               
         # Função para criar uma janela
-        def create_window(title):
-            window = MotionCaptureWindow(title)
+        def create_window(experiment):
+            window = MotionCaptureWindow(experiment)
             window.show()
-            windows.append(window)  # Armazena a referência para evitar garbage collection
+            self.windows.append(window)  # Armazena a referência para evitar garbage collection
 
         
         if self.currentExperimentList is not None:
             for experiment in self.currentExperimentList: 
-                thread = WorkerThread(f"Window {len(windows) + 1}")
+                thread = WorkerThread(experiment)
                 thread.create_window_signal.connect(create_window)
                 self.threads.append(thread)
                 thread.start() 
+            self.open_experiment_btn.setEnabled(False)
+            self.start_btn.setEnabled(True)
+            self.stop_btn.setEnabled(False)
         else:
             QMessageBox.warning(self, "Warning", "No experiment windows to start.")
-            self.start_btn.setEnabled(False)
+            self.open_experiment_btn.setEnabled(False)
             return
+
+    def start_experiment(self):
+        """Start the experiment by starting capturing."""
         
+        for window in self.windows:
+            window.start_capture()
+        self.start_btn.setEnabled(False)
+        self.stop_btn.setEnabled(True)
+        self.close_btn.setEnabled(False)
 
-
+    def stop_experiment(self):
+        """Stop the experiment by stopping capturing."""
         
+        for window in self.windows:
+            window.stop_capture()
+        self.stop_btn.setEnabled(False)
+        self.start_btn.setEnabled(False)
+        self.close_btn.setEnabled(True)
 
+    def close_experiment(self):
+        """close the experiment by closing windows and clearing current experiment."""
+            
+        # Close all open windows
+        for window in self.windows:
+            window.close()
+        self.windows.clear()
+
+        # Clear the current experiment list
+        self.currentExperimentList = None
+
+        # Remove all widgets (experiment labels) from experimentResources_layout
+        experimentResources_layout = self.open_experiment_btn.parentWidget().layout()
+        while experimentResources_layout.count():
+            item = experimentResources_layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()  # Properly delete the widget
+
+        self.currentExperimentList = None
+        self.stop_btn.setEnabled(False)
+        self.start_btn.setEnabled(False)
+        self.close_btn.setEnabled(False)
+        self.open_experiment_btn.setEnabled(False)
+
+    def refresh_cameras(self):
+        """Refresh the list of available cameras."""
+        current_camera = self.camera_combo.currentIndex()
+        self.camera_combo.clear()
+        self.camera_combo.addItems(get_available_cameras())
+        if current_camera < self.camera_combo.count():
+            self.camera_combo.setCurrentIndex(current_camera)
 
     def generate_filename(self):
         """Generate automatic filename with timestamp."""
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         documents_path = os.path.join(os.path.expanduser("~"), "Documents")
-        self.filename = os.path.join(documents_path, f"motion_capture_{timestamp}.csv")
+        self.filename = os.path.join(documents_path, f"motion_capture_{self.camera_combo.currentText()}_{timestamp}.csv")
         self.filename_label.setText(f"File: {self.filename}")
-        # self.start_btn.setEnabled(True)
+        # self.open_experiment_btn.setEnabled(True)
     
